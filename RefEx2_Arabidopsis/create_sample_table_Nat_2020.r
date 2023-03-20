18311c33c29a52e8e1aa1b9b023066d5bb6dbbfe
ktamura2021 <tamurak@hiroshima-u.ac.jp>
2023-03-20 13:11:10 +0900

    update sample tables

--- RefEx2_Arabidopsis/create_sample_table_Nat_2020.r
+++ RefEx2_Arabidopsis/create_sample_table_Nat_2020.r
@@ -7,7 +7,26 @@ df_select <- df %>% select(Run, Age, BioSample, Developmental_stage, Ecotype, `E
 
 df_select <- df_select %>% arrange(BioSample)
 
-write_csv(df_select, "sample_table_Arabidopsis_Nat_2020.csv")
+# Extract BioSample
+df_biosample <- df_select %>% select(BioSample) %>% distinct()
+# Create RefexSampleId
+df_refexsampleid <- paste("RES0000", seq(3791, 3791 + 55), sep = "") %>% as_tibble_col(column_name = "RefexSampleId")
+# Join two columns
+df_biosample <- bind_cols(df_biosample, df_refexsampleid)
+
+# Add RefexSampleId to the sample table
+df_select_refexsampleid <- df_select %>% left_join(df_biosample, by = "BioSample") %>% relocate(RefexSampleId)
+# Count NumberOfSamples
+df_count <- df_select_refexsampleid %>% count(RefexSampleId)
+# Add NumberOfSamples
+df_select_refexsampleid <- df_select_refexsampleid %>% left_join(df_count, by = "RefexSampleId") %>% rename(NumberOfSamples = n) %>% relocate(RefexSampleId, NumberOfSamples)
+
+# Create grouped sample table
+df_grouped <- df_select_refexsampleid %>% select(-Run) %>% distinct() %>% write_tsv("Arabidopsis_Nat_2020_refextable_grouped_sample.tsv")
+# Create sampleID mapping
+df_idmap <- df_select_refexsampleid %>% select(RefexSampleId, Run) %>% distinct() %>% write_tsv("Arabidopsis_Nat_2020_refextable_sampleID_mapping.tsv")
+
+
 
 sessionInfo()
 
--- RefEx2_Arabidopsis/create_sample_table_TPJ_2016.r
+++ RefEx2_Arabidopsis/create_sample_table_TPJ_2016.r
@@ -8,7 +8,24 @@ df <- bind_rows(df1, df2)
 df_select <- df %>% select(Run, BioSample, Ecotype, Organism, `Sample Name`, Tissue, dev_stage, Age, Treatment)
 df_select <- df_select %>% arrange(BioSample)
 
-write_csv(df_select, "sample_table_Arabidopsis_TPJ_2016.csv")
+# Extract BioSample
+df_biosample <- df_select %>% select(BioSample) %>% distinct()
+# Create RefexSampleId
+df_refexsampleid <- paste("RES0000", seq(3706, 3706 + 84), sep = "") %>% as_tibble_col(column_name = "RefexSampleId")
+# Join two columns
+df_biosample <- bind_cols(df_biosample, df_refexsampleid)
+
+# Add RefexSampleId to the sample table
+df_select_refexsampleid <- df_select %>% left_join(df_biosample, by = "BioSample") %>% relocate(RefexSampleId)
+# Count NumberOfSamples
+df_count <- df_select_refexsampleid %>% count(RefexSampleId)
+# Add NumberOfSamples
+df_select_refexsampleid <- df_select_refexsampleid %>% left_join(df_count, by = "RefexSampleId") %>% rename(NumberOfSamples = n) %>% relocate(RefexSampleId, NumberOfSamples)
+
+# Create grouped sample table
+df_grouped <- df_select_refexsampleid %>% select(-Run) %>% distinct() %>% write_tsv("Arabidopsis_TPJ_2016_refextable_grouped_sample.tsv")
+# Create sampleID mapping
+df_idmap <- df_select_refexsampleid %>% select(RefexSampleId, Run) %>% distinct() %>% write_tsv("Arabidopsis_TPJ_2016_refextable_sampleID_mapping.tsv")
 
 sessionInfo()
 
